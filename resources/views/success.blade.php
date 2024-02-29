355cdd68f37897693dfde4eb5c4b0f09d800ccea
Ziad <89700774+ZxProgramming@users.noreply.github.com>
2024-02-29 14:31:09 +0200

    done

--- resources/views/success.blade.php
+++ resources/views/success.blade.php
@@ -8,15 +8,10 @@
 @endif
 
 @if (session()->has('faild'))
-<div class="bs-toast toast fade show" role="alert" aria-live="assertive" aria-atomic="true">
-    <div class="toast-header bg-danger">
-        <img src="../../assets/img/avatars/1.png" class="d-block w-px-20 h-auto rounded me-2" alt="">
-        <div class="me-auto fw-semibold">Bootstrap</div>
-        <small>{{ date('Y-m-d') }}</small>
-        <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
-    </div>
-    <h1> {{session()->get('faild')}}</h1>
-</div>  
+<div class="alert alert-danger text-center" role="alert">
+    {{session()->get('faild')}}
+</div>
+
 
 
 
